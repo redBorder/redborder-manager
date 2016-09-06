bffb0f854a4ea262be18e2afc42aa52a74f37f63
cjmateos <mateos83@gmail.com>
2016-09-06 17:52:06 +0200

    Include manager functions in rb_set_mode

--- resources/bin/rb_set_mode.rb
+++ resources/bin/rb_set_mode.rb
@@ -15,11 +15,43 @@
 ## along with redBorder. If not, see <http://www.gnu.org/licenses/>.
 ########################################################################
 
-#require 'rubygems'
 require 'chef'
 require 'json'
+require '/usr/lib/redborder/bin/rb_config_utils.rb'
 
-load '/usr/lib/redborder/bin/rb_manager_functions.rb'
+def set_mode(hostname, mode)
+
+  if Config_utils.check_mode(mode)
+    # Load Chef configuration
+    Chef::Config.from_file("/etc/chef/client.rb")
+    Chef::Config[:node_name]  = "admin"
+    Chef::Config[:client_key] = "/etc/chef/admin.pem"
+    Chef::Config[:http_retry_count] = 5
+
+    # Load role
+    role = Chef::Role.load(hostname)
+
+    # Override redborder attribute
+    role.override_attributes["redborder"] = {} if role.override_attributes["redborder"].nil?
+
+    unless mode.nil?
+      # Set mode in role
+      role.override_attributes["redborder"]["mode"] = mode
+    end
+
+    # Save changes in role
+    if role.save
+      printf("INFO: %-50s %s\n", "#{hostname} passed to mode: #{mode}")
+    else
+      printf "Usage: rb_set_mode.rb master|custom [manager1] [manager2] [....]\n"
+    end
+  end
+  
+end
+
+##################
+# MAIN EXECUTION #
+##################
 
 managers=[]
 
@@ -27,9 +59,10 @@ if ARGV.length > 0
   mode=ARGV[0]
   managers=ARGV[1..ARGV.length-1]
 else
-  mode="unknown"
+  mode="custom"
 end
 
+# Only one manager to configure
 managers=[`hostname -s`.chomp] if managers.size==0
 
 managers.each do |hostname|
