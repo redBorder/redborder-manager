bc2a7329b2bc48d67fb34b169bbb2e8a148115b4
Alberto Rodriguez <albertrc.93@gmail.com>
2016-09-07 12:42:07 +0200

    Improved ipv4 check

--- resources/bin/rb_config_utils.rb
+++ resources/bin/rb_config_utils.rb
@@ -16,22 +16,39 @@ module Config_utils
         return mode_list.include?(mode)
     end
 
-    # Function to check a valid IPv4 IP address
-    def Config_utils.check_ipv4(ipv4)
-      ret = true
-      begin
-        x = NetAddr::CIDRv4.create("#{ipv4[:ip].nil? ? "0.0.0.0" : ipv4[:ip]}/#{ipv4[:netmask].nil? ? "255.255.255.255" : ipv4[:netmask]}")
-      rescue NetAddr::ValidationError => e
-        # error: netmask incorrect
-        ret = false
-      rescue => e
-        # general error"
-        ret = false
-      end
-      ret
+    #TODO
+    def Config_utils.check_hostname(hostname)
+        return true
     end
 
-   # Functon to chefk a valid domain. Based on rfc1123 and sethostname().
+    # Function to check a valid IPv4 IP address
+    # ipv4 parameter can be a hash with two keys:
+    # - :ip -> ip to be checked
+    # - :netmask -> mask to be checked
+    # Or can be a string with CIDR or standard notation.
+    def Config_utils.check_ipv4(ipv4)
+        ret = true
+        begin
+            # convert ipv4 from string format "192.168.1.0/255.255.255.0" into hash {:ip => "192.168.1.0", :netmask => "255.255.255.0"}
+            if ipv4.class == String
+                unless ipv4.match(/^(?<ip>\d+\.\d+\.\d+\.\d+)\/(?<netmask>(?:\d+\.\d+\.\d+\.\d+)|\d+)$/).nil?
+                    ipv4 = ipv4.match(/^(?<ip>\d+\.\d+\.\d+\.\d+)\/(?<netmask>(?:\d+\.\d+\.\d+\.\d+)|\d+)$/)
+                else
+                    ret = false
+                end
+            end
+            x = NetAddr::CIDRv4.create("#{ipv4[:ip].nil? ? "0.0.0.0" : ipv4[:ip]}/#{ipv4[:netmask].nil? ? "255.255.255.255" : ipv4[:netmask]}")
+        rescue NetAddr::ValidationError => e
+            # error: netmask incorrect
+            ret = false
+        rescue => e
+            # general error
+            ret = false
+        end
+        ret
+    end
+
+   # Function to chefk a valid domain. Based on rfc1123 and sethostname().
    # Suggest rfc1178
    # Max of 253 characters with hostname
    def Config_utils.check_domain(domain)
