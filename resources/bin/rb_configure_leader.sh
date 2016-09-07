eeac571f41ef67e399ba0d91e4bfc19574a2137d
cjmateos <mateos83@gmail.com>
2016-09-07 13:47:34 +0200

    fix in role creation

--- resources/bin/rb_configure_leader.sh
+++ resources/bin/rb_configure_leader.sh
@@ -331,7 +331,7 @@ function configure_leader(){
 
   # Create specific role for this node
   e_title "Creating custom chef role"
-  cp /var/chef/data/role/manager_node.json /var/chef/data/role/$(hostname -s).json
+  mv /var/chef/data/role/manager_node.json /var/chef/data/role/$(hostname -s).json
   # Change hostname in new role
   sed -i "s/manager_node/$(hostname -s)/g" /var/chef/data/role/$(hostname -s).json
 
@@ -350,7 +350,7 @@ function configure_leader(){
   # Save into cache directory
   e_title "Uploading cookbooks"
   mkdir -p /var/chef/cache/cookbooks/
-  listCookbooks="zookeeper kafka druid nomad http2k cron compat_resource logrotate chef-client" # The order matters!
+  listCookbooks="zookeeper kafka druid nomad http2k cron compat_resource logrotate chef-client rb-manager" # The order matters!
   for n in $listCookbooks; do # cookbooks
     rsync -a /var/chef/cookbooks/${n}/ /var/chef/cache/cookbooks/$n
     # Uploadind cookbooks
