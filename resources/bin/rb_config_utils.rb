27d7f749c43931e41df1010ce37cacc2718d9156
Juan J. Prieto <root@sdk7.redborder.lan>
2016-09-07 05:49:58 +0200

    deleted method check_hostname that always return true

--- resources/bin/rb_config_utils.rb
+++ resources/bin/rb_config_utils.rb
@@ -8,17 +8,13 @@ require 'netaddr'
 MODELIST_PATH="/usr/lib/redborder/mode-list.yml"
 
 module Config_utils
+
     #Function to check if mode is valid (if defined in mode-list.yml)
     #Returns true if it's valid and false if not
     #TODO: protect from exception like file not found
     def Config_utils.check_mode(mode)
         mode_list = YAML.load_file(MODELIST_PATH)
         return mode_list.include?(mode)
-    end
-
-    #TODO
-    def Config_utils.check_hostname(hostname)
-        return true
     end
 
     # Function to check a valid IPv4 IP address
