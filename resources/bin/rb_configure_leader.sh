2691412b9d8aaf3730244ad9e06f6c147a70e3f2
cjmateos <mateos83@gmail.com>
2016-11-14 13:37:51 +0100

    Change chef-server nginx non ssl port

--- resources/bin/rb_configure_leader.sh
+++ resources/bin/rb_configure_leader.sh
@@ -291,6 +291,7 @@ fi
 
 # Set chef-server internal nginx port to 4443
 echo "nginx['ssl_port'] = 4443" >> /etc/opscode/chef-server.rb
+echo "nginx['non_ssl_port'] = 4480" >> /etc/opscode/chef-server.rb
 
 # Chef server initial configuration
 e_title "Configuring Chef-Server"
