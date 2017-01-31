c45fbefe4847270e1821bd1592b635bbdd98f87b
Alberto Rodríguez <arodriguezdlc@gmail.com>
2017-01-31 12:53:37 +0100

    New script to orchestrate redborder bootstrap

---
+++ resources/bin/rb_bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+#
+#
+
+serf-join /etc/serf/00first.json
+serf-choose-leader 				\
+	-c 	mode=chef|full			\
+	-r 	consul=ready			\
+	-t 	leader=wait				\
+	-l 	rb_bootstrap_leader.sh  \
+	-f  rb_bootstrap_common.sh
