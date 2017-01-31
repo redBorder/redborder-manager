b1bc3d923ae495ed55468b1bbab0b15ccf56f46c
Alberto Rodríguez <arodriguezdlc@gmail.com>
2017-01-31 17:22:24 +0100

    Add bootstrap leader script

---
+++ resources/scripts/rb_bootstrap_leader.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+#
+#
+#
+
+require 'yaml'
+
+#Check what services are necessary
+external_services = [ "s3", "postgresql" ]
+required_services = []
+
+initconf_file = ARGV[0] if !ARGV.empty?
+if !File.exist? initconf_file
+	puts "ERROR: can't open init conf file (#{initconf_file})"
+	exit(1)
+else
+	initconf = YAML.load_file(initconf_file)
+	external_services.each { |service|
+		if initconf.key? service
+			required_services << service
+		end		
+	}
+end
+
+#Set serf tags for required services
+required_services.each { |service|
+	system("serf tags -set #{service}_required=true")
+}
+
+#Execution of rb_common_bootstrap
+puts "INFO: execute common bootstrap script"
+IO.popen("rb_common_bootstrap.sh").readlines
+
+#Wait for external services tags
+required_services.each { |service|
+	count = 1
+	until system("serf members -status alive -tag #{service}=ready | grep -q #{service}=ready") do
+		puts "INFO: Waiting for #{service} to be ready... (#{count})"
+		count = count + 1
+		sleep 5
+	end
+}
+
+#Set tag leader=inprogress
+puts "INFO: setting leader tag to inprogress"
+system("serf tags -set leader=inprogress")
+#Clean required tags
+required_services.each { |service| 
+	system("serf tags -delete #{service}_required")
+}
+
+
+#Execute configure leader script
+puts "INFO: execute configure leader script"
+IO.popen("rb_configure_leader.sh").readlines
+
+#Set tag leader=ready
+puts "INFO: setting leader tag to ready"
+system("serf tags -set leader=ready")
+
