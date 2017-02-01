106313f772a0491740187d9ed5e5df0aae2d32b5
cjmateos <mateos83@gmail.com>
2017-02-01 13:09:08 +0100

    Add rb_common_bootstrap.sh script

---
+++ resources/bin/rb_common_bootstrap.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#
+# Function to get available modes for a service
+#
+function candidateModes() {
+  SERVICE=$1
+
+  s3_modes=(s3 full)
+  postgresql_modes=(postgresql full)
+
+  SERVICE_MODES=$(eval echo \$${SERVICE}_modes)
+}
+
+# Usage function
+function usage() {
+    echo
+    echo "$(basename $0) <OPTIONS>"
+    echo "  h) usage"
+    echo
+}
+
+##################################################
+# MAIN EXECUTION
+##################################################
+
+#Default parameter values
+SERF_BIN=serf
+LEADER_TAG="leader=wait"
+READY_TAG="consul=ready"
+REQUIRED_KEY="required"
+s3_TAG="s3_required=true"
+postgresql_TAG="postgresql_required=true"
+S3_CANDIDATES_TAG="mode=s3|full"
+PG_CANDIDATES_TAG="mode=postgres|full"
+SERVICE_MODES=""
+
+#Getting options
+while getopts "h" opt ; do
+  case $opt in
+    h) usage; exit 0;;
+  esac
+done
+
+leader_tag_key=$(echo $LEADER_TAG | cut -d '=' -f 1)
+
+#Check if leader_tag_key or READY_TAG exists in a node. If not, keep waiting...
+serf members | grep -q $leader_tag_key || serf members -tag $READY_TAG | grep -q $READY_TAG
+while [ "x$?" == "x1" ] ; do
+  echo "INFO: Waiting for a leader..."
+  sleep 5
+  serf members | grep -q $leader_tag_key || serf members -tag $READY_TAG | grep -q $READY_TAG
+done
+
+#Check if LEADER_TAG is set in a node
+serf members -tag $LEADER_TAG | grep -q $LEADER_TAG
+if [ "x$?" == "x0" ] ; then
+  counter=0
+  #Check if exists any REQUIRED_KEY in a node. If not, keep waiting...
+  serf members -tag $REQUIRED_KEY | grep -q $REQUIRED_KEY
+  while [ "x$?" == "x1" && $counter -le 10 ] ; do
+    echo "INFO: Waiting for a required tag..."
+    sleep 2
+    let counter=counter+1
+    serf members -tag $REQUIRED_KEY | grep -q $REQUIRED_KEY
+  done
+  [ $counter -gt 10 ] && echo "WARNING: Required key tag not found while leader is waiting" && exit 0
+
+  #Check if any of S3_TAG or PG_TAG are true
+  serf members -tag $s3_TAG | grep -q $s3_TAG || serf members -tag $postgres_TAG | grep -q $postgres_TAG
+  if [ "x$?" == "x0" ] ; then
+    serviceList = (s3 postgresql)
+    for service in ${serviceList[@]}; do
+      #Check if any service is required is required
+      serf members -tag $(eval echo \$${service}_TAG)=true | grep -q $(eval echo \$${service}_TAG)=true
+      if [ "x$?" == "x0" ] ; then
+        #Call choose-leader and set service tag to ready
+        serf-choose-leader -r leader=inprogress -t $service=inprogress -c mode=$service|full -l rb_configure_initial_$service.sh
+      fi
+    done
+  fi
+fi
