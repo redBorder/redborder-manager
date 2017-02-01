0d565acfa2de5020806d3932fc3879384b914a4f
Alberto Rodríguez <arodriguezdlc@gmail.com>
2017-02-01 16:43:59 +0100

    fix problems in rb_bootstrap

--- resources/bin/rb_bootstrap.sh
+++ resources/bin/rb_bootstrap.sh
@@ -3,11 +3,12 @@
 #
 #
 #
+source /etc/profile
 
 serf-join /etc/serf/00first.json
-serf-choose-leader 				\
-	-c 	mode=chef|full			\
-	-r 	consul=ready			\
-	-t 	leader=wait				\
-	-l 	rb_bootstrap_leader		\
-	-f  rb_bootstrap_common.sh
+serf-choose-leader.sh			\
+	-c 	"mode=chef|full"		\
+	-r 	"consul=ready"			\
+	-t 	"leader=wait"			\
+	-l 	"rb_bootstrap_leader /etc/redborder/rb_init_conf.yml"	\
+	-f  "rb_bootstrap_common.sh"
