b3506c46957dc9614a7f7b757c4e744dc8308d76
cjmateos <mateos83@gmail.com>
2017-02-13 13:16:42 +0100

    Chef init conf for local s3

--- resources/scripts/rb_init_chef.rb
+++ resources/scripts/rb_init_chef.rb
@@ -25,30 +25,38 @@ unless s3_conf.nil?
   s3_endpoint = s3_conf['endpoint']
   s3_bucket = s3_conf['bucket']
 
-  unless s3_access.nil? or s3_secret.nil?
-    # Check S3 connectivity
-    open("/root/.s3cfg-test", "w") { |f|
-      f.puts "[default]"
-      f.puts "access_key = #{s3_access}"
-      f.puts "secret_key = #{s3_secret}"
-      f.puts "host_base = #{s3_endpoint}"
-      f.puts "host_bucket = %(bucket)s.#{s3_endpoint}"
-    }
-    out = system("/usr/bin/s3cmd -c /root/.s3cfg-test ls s3://#{s3_bucket} &>/dev/null")
-    #File.delete("/root/.s3cfg-test")
-  else
-    out = system("/usr/bin/s3cmd ls s3://#{s3_bucket} &>/dev/null")
-  end
-  unless out
-    p err_msg = "ERROR: Impossible connect to S3. Please review #{INITCONF} file"
-    ret = 1
-  end
+  # CHECK S3 CONNECTIVITY
+  #unless s3_access.nil? or s3_secret.nil?
+  #  # Check S3 connectivity
+  #  open("/root/.s3cfg-test", "w") { |f|
+  #    f.puts "[default]"
+  #    f.puts "access_key = #{s3_access}"
+  #    f.puts "secret_key = #{s3_secret}"
+  #    f.puts "host_base = #{s3_endpoint}"
+  #    f.puts "host_bucket = %(bucket)s.#{s3_endpoint}"
+  #    f.puts "signature_v2 = True"
+  #    f.puts "use_https = True"
+  #  }
+  #  out = system("/usr/bin/s3cmd -c /root/.s3cfg-test ls s3://#{s3_bucket} &>/dev/null")
+  #  #File.delete("/root/.s3cfg-test")
+  #else
+  #  out = system("/usr/bin/s3cmd ls s3://#{s3_bucket} &>/dev/null")
+  #end
+  #unless out
+  #  p err_msg = "ERROR: Impossible connect to S3. Please review #{INITCONF} file"
+  #  ret = 1
+  #end
 
   # Create chef-server configuration file for S3
   open("/etc/redborder/chef-server-s3.rb", "w") { |f|
     f.puts "bookshelf['enable'] = false"
-    f.puts "bookshelf['vip'] = \"#{s3_endpoint}\""
-    f.puts "bookshelf['external_url'] = \"https://#{s3_endpoint}\""
+    if s3_bucket == "rbookshelf"
+      f.puts "bookshelf['vip'] = \"#{s3_bucket}.#{s3_endpoint}\""
+      f.puts "bookshelf['external_url'] = \"https://#{s3_bucket}.#{s3_endpoint}\""
+    else
+      f.puts "bookshelf['vip'] = \"#{s3_endpoint}\""
+      f.puts "bookshelf['external_url'] = \"https://#{s3_endpoint}\""
+    end
     f.puts "bookshelf['access_key_id'] = \"#{s3_access}\""
     f.puts "bookshelf['secret_access_key'] = \"#{s3_secret}\""
     f.puts "opscode_erchef['s3_bucket'] = \"#{s3_bucket}\""
