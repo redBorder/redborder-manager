b5dd7e3ddb731b48607ed4690eeff0f183333a2d
cjmateos <mateos83@gmail.com>
2017-02-21 12:07:50 +0100

    Update rb_configure_initial_s3.sh

--- resources/bin/rb_configure_initial_s3.sh
+++ resources/bin/rb_configure_initial_s3.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
+# redborder initial configuracion script for local S3
 
 echo "INFO: Executing rb_configure_initial_s3"
-sleep 30 #Temporary sleep to mock script (for testing)
+
+#Get CDOMAIN
+if [ -f /etc/redborder/cdomain ]; then
+  cdomain=$(cat /etc/redborder/cdomain)
+else
+  echo "ERROR: cdomain not defined. Plese run rb_setup_wizard first"
+  exit 1
+fi
+
+# Configure Riak using chef-solo
+echo "INFO: Executing riak cookbook (1)"
+chef-solo -c /var/chef/cookbooks/riak/solo/riak_solo.rb -j /var/chef/cookbooks/riak/solo/riak_solo1.json
+echo "INFO: Executing riak cookbook (2)"
+chef-solo -c /var/chef/cookbooks/riak/solo/riak_solo.rb -j /var/chef/cookbooks/riak/solo/riak_solo1.json
+echo "INFO: Executing riak cookbook (3)"
+chef-solo -c /var/chef/cookbooks/riak/solo/riak_solo.rb -j /var/chef/cookbooks/riak/solo/riak_solo2.json
+
+# Get S3 keys
+if [ -f /etc/redborder/s3user.json ]; then
+  access_key=$(cat /etc/redborder/s3user.json | jq .key_id -r)
+  secret_key=$(cat /etc/redborder/s3user.json | jq .key_secret -r)
+  if [ "x$access_key" == "x" -o "x$secret_key" == "x" ]; then
+    echo "ERROR: S3 keys not created yet. Check /etc/redborder/s3user.json"
+    exit 1
+  fi
+else
+  echo "ERROR: S3 keys not created yet. File s3user.json is not present"
+  exit 1
+fi
+
+# Create s3 YML config file
+cat > /etc/redborder/s3_init_conf.yml <<-_RBEOF_
+s3:
+  access_key: $access_key
+  secret_key: $secret_key
+  bucket: rbookshelf
+  endpoint: s3.$cdomain
+_RBEOF_
 
 cat > /etc/serf/s3_query.json <<-_RBEOF_
 {
