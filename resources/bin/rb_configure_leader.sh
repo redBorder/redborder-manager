cc0f987f335eae034aa949f4ed177ea32f3577c3
Miguel <manegron@redborder.com>
2021-12-10 14:34:34 +0000

    Adding rb-social to rb_configure_leader.sh

--- resources/bin/rb_configure_leader.sh
+++ resources/bin/rb_configure_leader.sh
@@ -258,7 +258,7 @@ function configure_leader(){
   # Save into cache directory
   e_title "Uploading cookbooks"
   mkdir -p /var/chef/cache/cookbooks/
-  listCookbooks="zookeeper kafka druid http2k memcached chef-server consul hadoop samza nginx geoip webui snmp rbmonitor ntp f2k logstash pmacct minio postgresql dswatcher events-counter rsyslog rb-manager " # The order matters!
+  listCookbooks="zookeeper kafka druid http2k memcached chef-server consul hadoop samza nginx geoip webui snmp rbmonitor ntp f2k logstash pmacct minio postgresql dswatcher events-counter rsyslog rb-manager rb-social" # The order matters!
   for n in $listCookbooks; do # cookbooks
     # rsync -a /var/chef/cookbooks/${n}/ /var/chef/cache/cookbooks/$n
     # Uploadind cookbooks
