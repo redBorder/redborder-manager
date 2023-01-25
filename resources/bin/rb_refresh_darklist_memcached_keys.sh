fa80055df740dd9c0493eefd16e6b36fb62b6e7e
Edu Reyes <94845004+EduaReyes@users.noreply.github.com>
2023-01-25 14:14:59 +0000

    Updated memcached darklist scripts (#68)
    * Updated memcached darklist scripts
    
    * Updated memcached darklist scripts and run rb_update_darklist.sh at the end of rb_configure_leader
    
    * Fixed typos
    
    * Improve readibility
    
    * Removed rb_update_darklist.sh from rb_configure_leader.sh
    
    Co-authored-by: vimesa <vimesa@redborder.com>

--- resources/bin/rb_refresh_darklist_memcached_keys.sh
+++ resources/bin/rb_refresh_darklist_memcached_keys.sh
@@ -3,14 +3,16 @@
 
 source /etc/profile.d/rvm.sh
 
-[ ! -f /tmp/memcached_pid ] && exit 0;
+DARK_LIST_FILE="/usr/share/darklist.json"
 
-PID=$(/usr/sbin/pidof memcached)
-[ $? -eq 1 ] && exit 0;
-
-OLD_PID=$(</tmp/memcached_pid)
-if [ $PID -ne $OLD_PID ];then
-  echo $PID > /tmp/memcached_pid
-  /usr/lib/redborder/bin/rb_refresh_darklist_memcached_keys &>/dev/null
+if [ ! -f DARK_LIST_FILE ]; then
+  /usr/lib/redborder/bin/rb_update_darklist.sh
 fi
-exit 0;
+
+entries=$(/usr/lib/redborder/scripts/red.rb memcached keys dark | grep -c darklist)
+
+
+
+if [ "$entries" -eq 0 ];then
+  /usr/lib/redborder/scripts/rb_refresh_darklist_memcached_keys.rb > /dev/null
+fi
--- resources/bin/rb_update_darklist.sh
+++ resources/bin/rb_update_darklist.sh
@@ -5,31 +5,30 @@ source /etc/profile.d/rvm.sh
 
 LOG_FILE="/var/log/rb_update_darklist.log"
 TMP_FILE="/tmp/darklist.json"
-DARKLIST_FILE="/usr/share/darklist.json"
+DARK_LIST_FILE="/usr/share/darklist.json"
 
 #Download the /tmp/darklist.json file
-java -cp /usr/lib/darklist-updated/darklist-updated.jar net.redborder.darklistupdate.DarklistService $TMP_FILE >> $LOG_FILE &> /dev/null
+java -cp /usr/lib/darklist-updated/darklist-updated.jar \
+      net.redborder.darklistupdate.DarklistService $TMP_FILE >> $LOG_FILE &> /dev/null
 
 # If the file was not download.. we exit
-if [ ! -f $TMP_FILE ]; then exit 0; fi
+if [ ! -f $TMP_FILE ]
+then
+  echo "The file was not downloaded" >> $LOG_FILE
+  exit 1
+fi
 
 # If there is a previous file..
 if [ -f $DARK_LIST_FILE ]; then
   #Check if the md5 changes..
   #If didnt change, dont do anything.. exit
-  if cmp --silent "$TMP_FILE" "$DARKLIST_FILE"; then
+  if [[ $(cmp --silent "$TMP_FILE" "$DARK_LIST_FILE") && "$1" != "-f" ]] #Flag -f force to update
+  then
     echo 'nothing to do!' >> $LOG_FILE
     exit 0;
   fi
 fi
 
-cp $TMP_FILE $DARKLIST_FILE >> $LOG_FILE
-/usr/lib/redborder/bin/rb_manager_utils.sh -c -n all -p $DARKLIST_FILE >> $LOG_FILE
-/usr/lib/redborder/bin/rb_refresh_darklist_memcached_keys &>/dev/null
-
-PID=$(/usr/sbin/pidof memcached)
-if [ $? -eq 0 ]; then
-  echo $PID > /tmp/memcached_pid
-fi
-
-exit 0;
+cp $TMP_FILE $DARK_LIST_FILE >> $LOG_FILE
+/usr/lib/redborder/bin/rb_manager_utils.sh -c -n all -p $DARK_LIST_FILE >> $LOG_FILE
+/usr/lib/redborder/scripts/rb_refresh_darklist_memcached_keys.rb &>/dev/null
