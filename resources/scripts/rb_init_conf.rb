5b110ac7c59aaa3e52f35dcad7e229277052fe27
vimesa <vimesa@redborder.com>
2023-08-01 14:52:43 +0100

    Change name of script

--- resources/scripts/rb_init_conf.rb
+++ resources/scripts/rb_init_conf.rb
@@ -258,12 +258,11 @@ if !network.nil? #Firewall rules are not needed in cloud environments
 end
 
 # Node reboot
-
 # add path in rc.local
-system (echo "/usr/lib/redborder/scripts/self_recovery.rb >> /etc/rc.d/rc.local")
-
+system (echo "/usr/lib/redborder/scripts/chef_server_reload.sh >> /etc/rc.d/rc.local")
 # modify permissions to rc.local
 system("chmod a+x /etc/rc.d/rc.local")
+
 
 # Upgrade system
 system('yum install systemd -y')
