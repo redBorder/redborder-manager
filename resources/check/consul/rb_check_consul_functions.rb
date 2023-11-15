74152ad870cd247690e4378d8ae14a698d251aa6
Luis Blanco <ljblanco@redborder.com>
2023-11-15 14:23:34 +0000

    helper function to check if service is registered

--- resources/check/consul/rb_check_consul_functions.rb
+++ resources/check/consul/rb_check_consul_functions.rb
@@ -1,3 +1,7 @@
 def get_consul_members_status
   `consul members | sed '1d' | awk '{print $1" "$2" "$3}'`.split("\n")
 end
+
+def service_registered?(service)
+  `curl -X GET http://localhost:8500/v1/catalog/service/#{service}`
+end
