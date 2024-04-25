79e9764a55f1523d3827c9093c8ce6235453955a
David Vanhoucke <dvanhoucke@redborder.com>
2024-04-25 12:10:10 +0000

    add rb-exporter cookbook in the list of cookbooks to upload

--- resources/bin/rb_configure_leader.sh
+++ resources/bin/rb_configure_leader.sh
@@ -348,7 +348,7 @@ function configure_leader(){
   listCookbooks="rb-selinux cron zookeeper kafka druid http2k memcached chef-server consul
                 hadoop samza nginx geoip webui snmp mongodb rbmonitor rbscanner
                 f2k logstash pmacct minio postgresql rbdswatcher rbevents-counter
-                rsyslog freeradius rbnmsp n2klocd rbale rbcep k2http rblogstatter rb-arubacentral rbcgroup rb-proxy
+                rsyslog freeradius rbnmsp n2klocd rbale rbcep k2http rblogstatter rb-arubacentral rbcgroup rb-exporter rb-proxy
                 snort barnyard2 rb-ips rbaioutliers rb-manager" # The order matters!
 
   for n in $listCookbooks; do # cookbooks
