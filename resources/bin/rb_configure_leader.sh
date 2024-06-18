5517acadece36c9cf6426d37454a44014cc43859
Miguel Alvarez <malvarez@redborder.com>
2024-06-18 14:33:18 +0100

    Add ClamAV to upload list

--- resources/bin/rb_configure_leader.sh
+++ resources/bin/rb_configure_leader.sh
@@ -378,7 +378,7 @@ function configure_leader(){
                 hadoop samza nginx geoip webui snmp mongodb rbmonitor rbscanner
                 f2k logstash pmacct minio postgresql rbdswatcher rbevents-counter
                 rsyslog freeradius rbnmsp n2klocd rbale rbcep k2http rblogstatter rb-arubacentral rbcgroup rb-exporter rb-proxy rb-postfix
-                keepalived snort barnyard2 rb-ips rbaioutliers rb-manager" # The order matters!
+                keepalived snort barnyard2 rb-ips rbaioutliers rb-clamav rb-manager" # The order matters!
 
   for n in $listCookbooks; do # cookbooks
     # rsync -a /var/chef/cookbooks/${n}/ /var/chef/cache/cookbooks/$n
