35b41597ceaa697821167da3838bc0a1cee71da8
Miguel Alvarez <malvarez@redborder.com>
2024-07-29 15:51:53 +0100

    Fix mitre matrix import

--- resources/tools/mitre.py
+++ resources/tools/mitre.py
@@ -23,33 +23,36 @@ data = []
 for tactic in attack.enterprise.tactics:
     tactic_data = {
         "Tactic": tactic.name,
-        "ID": tactic.tactic_id,
-        "Techniques": []
+        "ID": tactic.external_references[0].external_id,
+        "Techniques": [],
+        "Description": tactic.description
     }
-    
+
     for technique in tactic.techniques:
-        technique_id = technique.technique_id 
+        technique_id = technique.technique_id
         if "." not in technique_id:
             technique_data = {
                 "Technique": technique.name,
                 "ID": technique_id,
-                "Subtechniques": []
+                "Subtechniques": [],
+                "Description": technique.description
             }
-            
+
             if hasattr(technique, 'techniques'):
                 for subtechnique in technique.techniques:
                     subtechnique_id = subtechnique.technique_id
                     subtechnique_data = {
                         "Subtechnique": subtechnique.name,
                         "ID": subtechnique_id,
-                        "ParentID": subtechnique.technique_id
+                        "ParentID": subtechnique.technique_id,
+                        "Description": subtechnique.description
                     }
                     technique_data["Subtechniques"].append(subtechnique_data)
             else:
                 technique_data["Subtechniques"].append("No subtechniques found")
-            
+
             tactic_data["Techniques"].append(technique_data)
-    
+
     data.append(tactic_data)
 
 json_data = json.dumps(data, indent=4)
