0b3b9ef56eada09df25eec4c7714b0ada938248d
Miguel Alvarez <malvarez@redborder.com>
2024-07-29 13:29:17 +0100

    Add tools path

---
+++ resources/tools/mitre.py
@@ -0,0 +1,57 @@
+#######################################################################
+# Copyright (c) 2024 ENEO Tecnologia S.L.
+# This file is part of redBorder.
+# redBorder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# redBorder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with redBorder. If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+import json
+from pyattck import Attck
+
+attack = Attck()
+
+data = []
+
+for tactic in attack.enterprise.tactics:
+    tactic_data = {
+        "Tactic": tactic.name,
+        "ID": tactic.tactic_id,
+        "Techniques": []
+    }
+    
+    for technique in tactic.techniques:
+        technique_id = technique.technique_id 
+        if "." not in technique_id:
+            technique_data = {
+                "Technique": technique.name,
+                "ID": technique_id,
+                "Subtechniques": []
+            }
+            
+            if hasattr(technique, 'techniques'):
+                for subtechnique in technique.techniques:
+                    subtechnique_id = subtechnique.technique_id
+                    subtechnique_data = {
+                        "Subtechnique": subtechnique.name,
+                        "ID": subtechnique_id,
+                        "ParentID": subtechnique.technique_id
+                    }
+                    technique_data["Subtechniques"].append(subtechnique_data)
+            else:
+                technique_data["Subtechniques"].append("No subtechniques found")
+            
+            tactic_data["Techniques"].append(technique_data)
+    
+    data.append(tactic_data)
+
+json_data = json.dumps(data, indent=4)
+
+print(json_data)
