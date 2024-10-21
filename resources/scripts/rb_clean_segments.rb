5c8ad80fad01e2f3004070533e82d4a26233ad78
jnavarrorb <jnavarro@redborder.com>
2024-10-21 17:04:53 +0100

    Apply rule_set for each datasource with druid rules

--- resources/scripts/rb_clean_segments.rb
+++ resources/scripts/rb_clean_segments.rb
@@ -60,6 +60,8 @@ remove_only_indexCache = false
 if zk.exists? '/cleanDruidSegments'
   puts "Another node have the lock. Only remove local data..."
   remove_only_indexCache = true
+else
+  zk.create('/cleanDruidSegments', '', :ephemeral => false)
 end
 
 # PG connection
@@ -74,131 +76,138 @@ db.exec("SELECT * FROM druid_rules") do |result|
   result.each do |row|
     # Decode the payload of the rule
     decoded_payload =JSON.parse(row["payload"][2..-1].gsub(/../) { |pair| pair.hex.chr }) if row["payload"]
-    rules << decoded_payload unless decoded_payload.empty?
+    rules << {rules_set: decoded_payload, datasource: row["datasource"]} unless decoded_payload.empty?
   end
 end
   
-# Fast exit if rules are not correct
-if rules.empty?
-  puts 'Unacceptable druid rules format on PG'
-  exit
-elsif not rules.first.first["tieredReplicants"].nil?
-  tieredReplicants = rules.first.first.fetch("tieredReplicants", {}).fetch("_default_tier", 0).to_i
-  type = rules.first.first["type"]
+rules.each do |rule|
+  rules_set = rule[:rules_set]
+  datasource = rule[:datasource]
+
+  puts "Segments from datasource: #{datasource}"
+  # Fast exit if rules are not correct
+  if rules_set.empty?
+    puts "Unacceptable druid rules format on PG"
+    next
+  elsif not rules_set.first["tieredReplicants"].nil?
+    tieredReplicants = rules.first.fetch("tieredReplicants", {}).fetch("_default_tier", 0).to_i
+    type = rules.first["type"]
+      
+    #if tieredReplicants == 1 and type == "loadForever"
+    if type == "loadForever"
+      puts "No segments must be removed because druid period is 'forever'"
+      next
+    end
+  end
     
-  #if tieredReplicants == 1 and type == "loadForever"
-  if type == "loadForever"
+  # Get default tier data and the period to mantain the data
+  defaultTier = rules_set.select{|x| 
+                            #x["tier"] == "_default_tier" if !x["tier"].nil?
+                            x["tieredReplicants"].first.first == "_default_tier" if !x["tieredReplicants"].nil?                        
+                            }.first
+  puts "defaultTier is #{defaultTier}"
+  if defaultTier.nil?
+    puts "No default tier exists on PG. Exiting..."
+    next
+  end
+    
+    
+  period = defaultTier["period"].upcase
+  periodInSecs = ISO8601::Duration.new(period).to_seconds
+  limitDate = Time.now - periodInSecs
+  puts "limitDate for datasource #{datasource} is #{limitDate}"
+    
+  if period == "P5000Y"
     puts "No segments must be removed because druid period is 'forever'"
-    exit
+    next
   end
-end
-  
-# Get default tier data and the period to mantain the data
-defaultTier = rules.first.select{|x| 
-                           #x["tier"] == "_default_tier" if !x["tier"].nil?
-                           x["tieredReplicants"].first.first == "_default_tier" if !x["tieredReplicants"].nil?                        
-                          }.first
-puts "defaultTier is #{defaultTier}"
-if defaultTier.nil?
-  puts "No default tier exists on PG. Exiting..."
-  exit
-end
-  
-  
-period = defaultTier["period"].upcase
-periodInSecs = ISO8601::Duration.new(period).to_seconds
-limitDate = Time.now - periodInSecs
-puts "limitDate is #{limitDate}"
-  
-if period == "P5000Y"
-  puts "No segments must be removed because druid period is 'forever'"
-  exit
-end
 
-if !remove_only_indexCache
-  # Create the lock
-  path = zk.create('/cleanDruidSegments', ephemeral: true)
-  
-  puts "Deleting segments older than #{limitDate} (Period #{period})"
-  
-  # Get all the segments from PG
-  segments_to_delete_from_pg = []
-  db.exec("SELECT * FROM druid_segments") do |result|
-    result.each do |row|
-      date = Time.parse row.values_at('start').first
-      if date < limitDate
-        segments_to_delete_from_pg << row
+  if !remove_only_indexCache
+    # Create the lock
+    path = zk.create("/cleanDruidSegments/#{datasource}", ephemeral: true)
+    
+    puts "Deleting segments from #{datasource} older than #{limitDate} (Period #{period})"
+    
+    # Get all the segments from PG
+    segments_to_delete_from_pg = []
+    db.exec("SELECT * FROM druid_segments WHERE datasource = '#{datasource}'") do |result|
+      result.each do |row|
+        date = Time.parse row.values_at('start').first
+        if date < limitDate
+          segments_to_delete_from_pg << row
+        end
       end
     end
-  end
-  
-  # Remove segments from PG
-  if segments_to_delete_from_pg.size > 0
-    puts "#{segments_to_delete_from_pg.size} segments marked for removing on PG"
-  
-    segments_to_delete_from_pg.each do |segment|
-      puts "Removing PG segment id #{segment['id']}"
-  
-      # Remove it from PG
-      db.exec("DELETE FROM druid_segments WHERE id = '#{segment['id']}'")
-    end
-  else
-    puts "No segments must be removed from PG"
-  end
-  
-  # Remove segments from S3 if necessary
-  if File.exist? "/var/www/rb-rails/config/aws.yml"
-    s3_config = YAML.load_file("/var/www/rb-rails/config/aws.yml")
-    s3 = Aws::S3::Client.new(access_key_id: s3_config["production"]["access_key_id"],
-      secret_access_key: s3_config["production"]["secret_access_key"],
-      region: 'us-east-1',
-      endpoint: s3_config["production"]["s3_protocol"] +"://"+ s3_config["production"]["s3_host_name"]
-       )
-    bucket_name = s3_config["production"]["bucket"].chomp!('/')
-
-  
-    # Get all the segments from S3
-    segments_to_delete_from_s3 = []
-    segments_on_s3 = []
-    continuation_token = nil
-
-    begin
-      response = s3.list_objects_v2(bucket: bucket_name, prefix: "rbdata/",continuation_token: continuation_token)
-      segments_on_s3.concat(response.contents.map(&:key))
-      continuation_token = response.next_continuation_token
-    end while continuation_token
-
-    # Filter by date
-    segments_on_s3.each do |segment|
-      date = Time.parse segment.split("/")[2].split("_")[0]
-      if date < limitDate
-        segments_to_delete_from_s3 << segment
-      end
-
-    end
-    #end
-    # Remove segments from S3
-    if segments_to_delete_from_s3.size > 0
-      puts "#{segments_to_delete_from_s3.size} objects marked for removing on S3"
-      segments_to_delete_from_s3.each do |object_key|
-        puts "Removing S3 object with path #{object_key}"
-        # Delete the object
-        s3.delete_object(bucket: bucket_name, key: object_key)
+    
+    # Remove segments from PG
+    if segments_to_delete_from_pg.size > 0
+      puts "#{segments_to_delete_from_pg.size} segments marked for removing on PG"
+    
+      segments_to_delete_from_pg.each do |segment|
+        puts "Removing PG segment id #{segment['id']}"
+    
+        # Remove it from PG
+        db.exec("DELETE FROM druid_segments WHERE id = '#{segment['id']}'")
       end
     else
-      puts "No segments must be removed from S3"
+      puts "No segments must be removed from PG"
     end
+    
+    # Remove segments from S3 if necessary
+    if File.exist? "/var/www/rb-rails/config/aws.yml"
+      s3_config = YAML.load_file("/var/www/rb-rails/config/aws.yml")
+      s3 = Aws::S3::Client.new(access_key_id: s3_config["production"]["access_key_id"],
+        secret_access_key: s3_config["production"]["secret_access_key"],
+        region: 'us-east-1',
+        endpoint: s3_config["production"]["s3_protocol"] +"://"+ s3_config["production"]["s3_host_name"]
+        )
+      bucket_name = s3_config["production"]["bucket"].chomp!('/')
+
+    
+      # Get all the segments from S3
+      segments_to_delete_from_s3 = []
+      segments_on_s3 = []
+      continuation_token = nil
+
+      begin
+        response = s3.list_objects_v2(bucket: bucket_name, prefix: "rbdata/#{datasource}/",continuation_token: continuation_token)
+        segments_on_s3.concat(response.contents.map(&:key))
+        continuation_token = response.next_continuation_token
+      end while continuation_token
+
+      # Filter by date
+      segments_on_s3.each do |segment|
+        date = Time.parse segment.split("/")[2].split("_")[0]
+        if date < limitDate
+          segments_to_delete_from_s3 << segment
+        end
+
+      end
+      #end
+      # Remove segments from S3
+      if segments_to_delete_from_s3.size > 0
+        puts "#{segments_to_delete_from_s3.size} objects marked for removing on S3"
+        segments_to_delete_from_s3.each do |object_key|
+          puts "Removing S3 object with path #{object_key}"
+          # Delete the object
+          s3.delete_object(bucket: bucket_name, key: object_key)
+        end
+      else
+        puts "No segments must be removed from S3"
+      end
+    end
+
+    # Remove zk node
+    # zk.delete("/clean_segments/barrier")
+    zk.delete path
   end
 
-  # Remove zk node
-  # zk.delete("/clean_segments/barrier")
-  zk.delete path
-  zk.close!
+  # Remove segments from historical indexCache
+  puts "Removing files from druid historical indexCache"
+  removeFiles("/var/druid/historical/indexCache/#{datasource}/*", limitDate)
+  # Remove segments from localStorage
+  puts "Removing files from localStorage"
+  removeFiles("/var/druid/data/#{datasource}/*", limitDate)
 end
-
-# Remove segments from historical indexCache
-puts "Removing files from druid historical indexCache"
-removeFiles("/var/druid/historical/indexCache/*/*", limitDate)
-# Remove segments from localStorage
-puts "Removing files from localStorage"
-removeFiles("/var/druid/data/*/*", limitDate)
+zk.delete ('/cleanDruidSegments')
+zk.close!
