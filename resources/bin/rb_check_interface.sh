2ec7093fe51b345781ad5e981e8817f829655161
nilsver <nverschaeve@redborder.com>
2024-11-22 12:51:14 +0000

    fix call in check_interface

--- resources/bin/rb_check_interface.sh
+++ resources/bin/rb_check_interface.sh
@@ -5,11 +5,17 @@ check_sync_interface_status() {
     local sync_ip=$2
 
     if ! ping -c 1 "$sync_ip" &> /dev/null; then
+        echo "Sync interface down for $manager, restarting keepalived..."
         systemctl restart keepalived
     fi
 }
 
-input_json=$1
+if [[ -z "$1" ]]; then
+    echo "Error: JSON input is required."
+    exit 1
+fi
+
+input_json="$1"
 
 for manager in $(echo "$input_json" | jq -r 'keys[]'); do
     sync_ip=$(echo "$input_json" | jq -r ".\"$manager\".ipaddress_sync")
