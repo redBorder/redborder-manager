1403930f976683fecc989dbba22e1582a5917d2f
Rafael Gomez <rgomez@redborder.com>
2025-02-07 08:22:38 +0000

    refactor(rb_sync_from_master.sh): improve PostgreSQL backup process on doing it in temp folder

--- resources/bin/rb_sync_from_master.sh
+++ resources/bin/rb_sync_from_master.sh
@@ -6,24 +6,22 @@ master=$1
 hostname=`hostname`
 
 if [ "x$master" == "x" ]; then
-    echo "need to provide master IP" 
+    echo "Need to provide master IP"
     exit 1
 fi
 
 rm -f /tmp/postgresql.trigger
-pushd /var/lib/pgsql
+backup_dir="/var/lib/pgsql/data_tmp"
+data_dir="/var/lib/pgsql/data"
 
-echo Stopping PostgreSQL
-sudo service postgresql stop
+# Ensure previous backup attempt directory is clean
+sudo -u postgres rm -rf "$backup_dir"
 
-echo Cleaning up old cluster directory
-sudo -u postgres rm -rf /var/lib/pgsql/data
-
-echo Starting base backup as replicator
+echo "Starting base backup as replicator to temporary directory..."
 retries=3
 count=0
 while [ $count -lt $retries ]; do
-    sudo -u postgres pg_basebackup -h $master -D /var/lib/pgsql/data -U rep -R -v 2> /tmp/pg_basebackup_error.log
+    sudo -u postgres pg_basebackup -h "$master" -D "$backup_dir" -U rep -R -v 2> /tmp/pg_basebackup_error.log
     if [ $? -eq 0 ]; then
         echo "pg_basebackup completed successfully."
         break
@@ -34,20 +32,28 @@ while [ $count -lt $retries ]; do
 done
 
 if [ $count -eq $retries ]; then
-    echo "pg_basebackup failed after $retries attempts. Check /tmp/pg_basebackup_error.log for details."
+    echo "pg_basebackup failed after $retries attempts. Keeping the existing data directory."
     cat /tmp/pg_basebackup_error.log
     exit 1
 fi
 
+echo "Stopping PostgreSQL"
+sudo systemctl stop postgresql
+
+echo "Replacing old data directory with new backup"
+sudo -u postgres rm -rf "$data_dir"
+sudo mv "$backup_dir" "$data_dir"
+
 echo "Creating standby.signal file"
-sudo -u postgres bash -c "touch /var/lib/pgsql/data/standby.signal"
+sudo -u postgres touch "$data_dir/standby.signal"
 
-[ -f /var/lib/pgsql/data/recovery.done ] && rm -f /var/lib/pgsql/data/recovery.done
+[ -f "$data_dir/recovery.done" ] && sudo rm -f "$data_dir/recovery.done"
 
-sed -i '/^primary_conninfo/d' /var/lib/pgsql/data/postgresql.conf
-sed -i '/^promote_trigger_file/d' /var/lib/pgsql/data/postgresql.conf
-sed -i '/^standby_mode/d' /var/lib/pgsql/data/postgresql.conf
-sudo -u postgres bash -c "cat >> /var/lib/pgsql/data/postgresql.conf <<- _EOF1_
+sed -i '/^primary_conninfo/d' "$data_dir/postgresql.conf"
+sed -i '/^promote_trigger_file/d' "$data_dir/postgresql.conf"
+sed -i '/^standby_mode/d' "$data_dir/postgresql.conf"
+
+sudo -u postgres bash -c "cat >> $data_dir/postgresql.conf <<- _EOF1_
 #standby_mode = 'on'
 primary_conninfo = 'host=$master port=5432 user=rep application_name=$hostname'
 promote_trigger_file = '/tmp/postgresql.trigger'
@@ -59,4 +65,3 @@ sudo service postgresql start
 
 echo "restart webui in all nodes"
 rbcli node execute all systemctl restart webui
-popd
