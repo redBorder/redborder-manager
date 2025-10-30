4b3fe9388119ff1a19fb8f6f41d5723572e4d290
Pablo Pérez <pperez@redborder.com>
2025-10-30 14:37:28 +0000

    Update pyyara.py to python 3

--- resources/tools/pyyara.py
+++ resources/tools/pyyara.py
@@ -6,9 +6,9 @@
 # licenses this file to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,72 +18,70 @@
 #  Copyright 2013 Endgame Inc.
 
 import sys
-sys.path.append('/opt/rb/lib/python2.6/site-packages/')
-sys.path.append('/opt/rb/lib64/python2.6/site-packages/')
 import os
 import glob
 import yara
 import json
-import codecs
 import datetime
 
-sys.stdin = os.fdopen(sys.stdin.fileno(), 'rb', 0)
-sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
-sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
-
 def log(msg):
-    sys.stderr.write("[%s] "%datetime.datetime.now())
-    sys.stderr.write(msg)
-    sys.stderr.write("\n")
+    sys.stderr.write("[%s] " % datetime.datetime.now())
+    sys.stderr.write(msg + "\n")
     sys.stderr.flush()
 
 def output(msg):
-    sys.stdout.write(msg)
-    sys.stdout.write("\n")
+    sys.stdout.write(msg + "\n")
     sys.stdout.flush()
 
 def die():
     log("Process timed out, about to exit ...")
-    print json.dumps({"_error":"timed out"})
+    print(json.dumps({"_error":"timed out"}))
     sys.exit(1)
 
-log("PID=%d"%os.getpid())
-log("PARENT PID=%d"%os.getppid())
-log("CWD=%s"%os.getcwd())
+log("PID=%d" % os.getpid())
+log("PARENT PID=%d" % os.getppid())
+log("CWD=%s" % os.getcwd())
 
 if len(sys.argv) > 2:
-    path_yara_rules=sys.argv[2]
+    path_yara_rules = sys.argv[2]
 else:
-    path_yara_rules="yara_rules/"
+    path_yara_rules = "yara_rules/"
 
 start = datetime.datetime.now()
 sigs = dict([(name.replace(".yara", "").split("/")[-1], name) for name in glob.glob(path_yara_rules+"*.yar*")])
 rules = yara.compile(filepaths=sigs)
 end = datetime.datetime.now()
-log("Loaded yara rules in %s: %s"%( end-start, json.dumps(sigs, indent=4)))
+log("Loaded yara rules in %s: %s"%( end - start, json.dumps(sigs, indent=4)))
 
 matches = {}
 
 def match_callback(data):
     if data.get("matches", False):
-        del data["matches"]
-        if "strings" in data: del data["strings"]
-        if "rule" in data: data["rule"] = unicode(data["rule"], errors='ignore')
-        if "tags" in data and not data["tags"]: del data["tags"]
-        if "meta" in data and "description" in data["meta"]:
-            data["meta"]["description"] = unicode(data["meta"]["description"], errors='ignore')
+        data.pop("matches")
+        if "strings" in data:
+            data.pop("strings")
+        if "rule" in data and isinstance(data["rule"], bytes):
+            data["rule"] = data["rule"].decode('utf-8', errors='ignore')
+        if "tags" in data and not data["tags"]:
+            data.pop("tags")
+        if "meta" in data and "description" in data["meta"] and isinstance(data["meta"]["description"], bytes):
+            data["meta"]["description"] = data["meta"]["description"].decode('utf-8', errors='ignore')
         matches['matches'].append(data)
     return yara.CALLBACK_CONTINUE
 
-log("Openning %s for reading ..."%(sys.argv[1]))
-data = open(sys.argv[1], 'rb').read()
+# Open and read the target file
+target_file = sys.argv[1]
+
+log("Openning %s for reading ..."%(target_file))
+data = open(target_file, 'rb').read()
+
 log("Performing matching on %d bytes of data ..."%len(data))
-matches = {'filename': os.path.basename(sys.argv[1]), 'matches':[]}
+matches = {'filename': os.path.basename(target_file), 'matches':[]}
 
 start = datetime.datetime.now()
 rules.match(data=data, callback=match_callback)
 end = datetime.datetime.now()
 
-log("Done matching %d bytes in %s, printing results ..."%(len(data), end-start))
+log("Done matching %d bytes in %s, printing results ..."%(len(data), end - start))
 output(json.dumps(matches))
 log("Process Exiting")
