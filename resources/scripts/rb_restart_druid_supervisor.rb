1e5d2367446c538b5c6b11460cf924ac49e9dd4d
ljblancoredborder <ljblanco@redborder.com>
2025-11-21 15:59:10 +0000

    make cluster sensitive using zookeeper and druid router instead of just query to localhost

--- resources/scripts/rb_restart_druid_supervisor.rb
+++ resources/scripts/rb_restart_druid_supervisor.rb
@@ -16,55 +16,97 @@
 ## along with redBorder. If not, see <http://www.gnu.org/licenses/>.
 ########################################################################
 
+require 'zk'
 require 'net/http'
 require 'uri'
 require 'json'
 require 'optparse'
 
 def usage
-  p 'rb_restart_druid_supervisor.rb [-h] -s <supervisor_name>'
-  p '   -h             : print this help'
-  p '   -s <supervisor_name>: rb_monitor | rb_monitor_<UUID> | rb_vault (etc)'
-  p ' To get full list of active supervisors, execute rb_get_druid_supervisors'
+  puts 'rb_restart_druid_supervisor.rb [-h] -s <supervisor_name>'
+  puts '   -h                     : show help'
+  puts '   -s <supervisor_name>   : rb_monitor | rb_monitor_<UUID> | rb_vault | ...'
+  puts 'To list active supervisors, run: rb_get_druid_supervisors'
 end
 
-def post_to_supervisor(supervisor_name, action)
-  puts "On #{action} to supervisor #{supervisor_name}..."
-
-  druid_port = 8081
-  url = "http://localhost:#{druid_port}/druid/indexer/v1/supervisor"
+# ----------------------------------------------------------
+#  Resolve a Druid Router host/port from ZooKeeper
+# ----------------------------------------------------------
+def resolve_druid_router
   begin
-    uri = URI("#{url}/#{supervisor_name}/#{action}")
+    zk_hosts = 'zookeeper.service:2181'
+    zk = ZK.new(zk_hosts)
 
-    request = Net::HTTP::Post.new(uri)
-    request['Content-Type'] = 'application/json'
-    request.body = { feed: supervisor_name }.to_json
+    druid_router_path = '/druid/discoveryPath/druid:router'
+    raise "Router path '#{druid_router_path}' does not exist in Zookeeper" unless zk.exists?(druid_router_path)
 
-    _response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      http.request(request)
-    end
+    routers = zk.children(druid_router_path)
+    raise 'ERROR: No routers found. Please enable druid router at least in one node.' if routers.empty?
 
-    # puts response.body
-    puts "Action: #{action} to supervisor #{supervisor_name} done."
-  rescue e
-    puts "ERROR: #{action} to supervisor #{supervisor_name} failed."
+    # Pick one router randomly
+    router_id = routers.sample
+    data, _stat = zk.get("#{druid_router_path}/#{router_id}")
+
+    router_info = JSON.parse(data) || {}
+    raise 'ERROR on restart supervisor' unless router_info['address'] && router_info['port']
+
+    router_info
+  rescue => e
+    puts "ERROR on restart supervisor: #{e.message}"
+    exit 0
+  ensure
+    zk&.close
   end
 end
 
+# ----------------------------------------------------------
+#  POST action to supervisor via Druid Router
+# ----------------------------------------------------------
+def post_to_supervisor(supervisor_name, action)
+  r = resolve_druid_router
+  address = r['address'] # druid api address
+  port = r['port'] # druid api port
+
+  puts "Performing '#{action}' on supervisor '#{supervisor_name}' via #{address}:#{port}"
+
+  uri = URI("http://#{address}:#{port}/druid/indexer/v1/supervisor/#{supervisor_name}/#{action}")
+
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
+  request.body = { feed: supervisor_name }.to_json
+
+  response = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(request) }
+
+  unless response.is_a?(Net::HTTPSuccess)
+    raise "HTTP #{response.code} #{response.message} - #{response.body}"
+  end
+
+  puts "✔ Action '#{action}' completed successfully."
+rescue => e
+  puts "✖ ERROR performing '#{action}' on supervisor '#{supervisor_name}': #{e.message}"
+end
+
+# ----------------------------------------------------------
+#  MAIN
+# ----------------------------------------------------------
 options = {}
 OptionParser.new do |opts|
-  opts.on('-s VALUE', 'Specify supervisor name') { |v| options[:s] = v }
-  opts.on('-h', 'Display help') { options[:h] = true }
+  opts.on('-s VALUE', 'Supervisor name') { |v| options[:s] = v }
+  opts.on('-h', 'Help') { options[:h] = true }
 end.parse!
 
 if options[:h]
   usage
-elsif options[:s].nil?
-  usage
-  exit 1
-else
-  supervisor_name = options[:s]
-  post_to_supervisor(supervisor_name, 'suspend')
-  post_to_supervisor(supervisor_name, 'reset')
-  post_to_supervisor(supervisor_name, 'resume')
+  exit 0
 end
+
+if options[:s].nil?
+  usage
+  exit 0
+end
+
+supervisor_name = options[:s]
+
+post_to_supervisor(supervisor_name, 'suspend')
+post_to_supervisor(supervisor_name, 'reset')
+post_to_supervisor(supervisor_name, 'resume')
